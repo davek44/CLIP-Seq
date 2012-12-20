4843c3251207760f7c1dc75733ac8230c19533dc
David Kelley <dkelley@fas.harvard.edu>
2012-12-20 15:15:50 -0500

    Fix FPKM to lambda in test

--- test_clip_peaks.py
+++ test_clip_peaks.py
@@ -41,7 +41,7 @@ class TestConvoluteLambda(unittest.TestCase):
         junctions = []
         exon_counts = [4]*5
 
-        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size for ec in exon_counts]
+        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size/1000.0/1000000.0 for ec in exon_counts]
         code_lambda = self.compute_lambdas(len(gene),junctions)
 
         self.assertEqual(len(true_lambda),len(code_lambda))
@@ -55,7 +55,7 @@ class TestConvoluteLambda(unittest.TestCase):
         junctions = [7,10,12,18]
         exon_counts = [4,4,4,3,2,1,1,2,2,2,1,0,0,0,1,2,3]
 
-        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size for ec in exon_counts]
+        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size/1000.0/1000000.0 for ec in exon_counts]
         code_lambda = self.compute_lambdas(len(gene),junctions)
 
         self.assertEqual(len(true_lambda),len(code_lambda))
@@ -69,7 +69,7 @@ class TestConvoluteLambda(unittest.TestCase):
         junctions = [4,10,12,15]
         exon_counts = [3,2,1,0,0,0,1,2,2,2,1,1,2,3,4,4,4]
 
-        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size for ec in exon_counts]
+        true_lambda = [float(ec*(self.fpkm_exon+self.fpkm_pre) + (self.window_size-ec)*self.fpkm_pre)/self.window_size/1000.0/1000000.0 for ec in exon_counts]
         code_lambda = self.compute_lambdas(len(gene),junctions)
 
         self.assertEqual(len(true_lambda),len(code_lambda))
@@ -90,13 +90,14 @@ class TestWindows2Peaks(unittest.TestCase):
         self.tx.add_exon(1,10)
         self.tx.fpkm_exon = 1
         self.tx.fpkm_pre = 0
+        self.poisson_lambda = self.tx.fpkm_exon / 1000.0 / 1000000.0
 
     def test1(self):        
         window_counts = [3,4,6,4,2]
         window_stats = [(wc,.001) for wc in window_counts]
 
-        true_peaks = [(3,6,6,clip_peaks.scan_stat_approx3(6, self.window_size, self.txome_size, self.tx.fpkm_exon))]
-        code_peaks = pdb.runcall(clip_peaks.windows2peaks, self.read_midpoints, [], window_stats, self.window_size, .05, self.tx, self.txome_size)
+        true_peaks = [(3,6,6,clip_peaks.scan_stat_approx3(6, self.window_size, self.txome_size, self.poisson_lambda))]
+        code_peaks = clip_peaks.windows2peaks(self.read_midpoints, [], window_stats, self.window_size, .05, self.tx, self.txome_size)
 
         self.assertEqual(len(true_peaks),len(code_peaks))
         for i in range(len(true_peaks)):
