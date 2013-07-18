db4e3864985acc5aef93e1dfdfb2aa8cd63c1960
David Kelley <dkelley@fas.harvard.edu>
2013-07-18 15:42:12 -0400

    Rewrite wo pybedtools and w mapq and nh usage

--- mutation_profile.py
+++ mutation_profile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from optparse import OptionParser
-import pdb, string
-import pybedtools, pysam
+import os, pdb, string, subprocess, tempfile
+import pysam
 
 ################################################################################
 # mutation_profile.py
@@ -27,12 +27,10 @@ def main():
         bam_file = args[1]
 
     # filter BAM using features
-    bam_bt = pybedtools.BedTool(bam_file)
     if options.feature_file:
-        feature_bt = pybedtools.BedTool(options.feature_file)
-        bam_filt_bt = bam_bt.intersect(feature_bt, u=True, s=True, f=0.5)
-    else:
-        bam_filt_bt = bam_bt
+        bam_feat_fd, bam_feat_file = tempfile.mkstemp(dir='%s/research/scratch/temp' % os.environ['HOME'])
+        subprocess.call('intersectBed -abam %s -b %s > %s' % (bam_file, options.feature_file, bam_feat_file), shell=True)
+        bam_file = bam_feat_file
 
     # load reference fasta
     fasta = pysam.Fastafile(fasta_file)
@@ -40,71 +38,81 @@ def main():
     # count mutations and nt's
     mutation_profile = {}
     acgt_content = {'A':0, 'C':0, 'G':0, 'T':0}
-    for align in bam_filt_bt.features():
-        align_a = align.__str__().split('\t')
 
-        # get read sequence
-        align_seq = align_a[9].upper()        
+    bam_in = pysam.Samfile(bam_file, 'rb')
+    for aligned_read in bam_in:
+        if aligned_read.mapq > 0:
+            # get read sequence
+            align_seq = aligned_read.seq.upper()
 
-        # parse cigar string
-        cigar_str = align_a[5]
-        cigar_a = parse_cigar(cigar_str)
+            # correct for deleted nts
+            deleted_ref_nts = sum([nt_count for code, nt_count in aligned_read.cigar if code in [2,3]])
 
-        # load reference sequence
-        deleted_ref_nts = sum([nt_count for nt_count,code in cigar_a if code in ['D','N']])
-        ref_seq = fasta.fetch(reference=align.chrom, start=align.start, end=align.end+deleted_ref_nts).upper()
+            # get reference sequence
+            ref_seq = fasta.fetch(reference=bam_in.references[aligned_read.tid], start=aligned_read.pos, end=aligned_read.aend+deleted_ref_nts).upper()
 
-        strand = get_strand(int(align_a[1]))
-        #if strand == '-':
-        #    continue
+            # correct for multimap
+            nh_tag = float(aligned_read.opt('NH'))
 
-        # count nt's
-        for nt in ref_seq:
-            if nt != 'N':
-                if strand == '+':
-                    acgt_content[nt] += 1
-                else:
-                    acgt_content[rc(nt)] += 1
-
-        # process cigar string
-        ref_i = 0
-        align_i = 0
-        for nt_count, code in cigar_a:
-            # match
-            if code == 'M':
-                for j in range(nt_count):
-                    if ref_seq[ref_i+j] != align_seq[align_i+j]:
-                        if strand == '+':
-                            mut_key = (ref_seq[ref_i+j], align_seq[align_i+j])
+            ref_i = 0
+            align_i = 0
+            for code, nt_count in aligned_read.cigar:
+                # match
+                if code == 0:
+                    for j in range(nt_count):
+                        # acgt content
+                        if aligned_read.is_reverse:
+                            acgt_content[rc(ref_seq[ref_i+j])] += 1/nh_tag
                         else:
-                            mut_key = (rc(ref_seq[ref_i+j]), rc(align_seq[align_i+j]))
-                        mutation_profile[mut_key] = mutation_profile.get(mut_key,0) + 1
-                ref_i += nt_count
-                align_i += nt_count
+                            acgt_content[ref_seq[ref_i+j]] += 1/nh_tag
 
-            # insertion
-            elif code == 'I':
-                for j in range(nt_count):
-                    if strand == '+':
-                        mut_key = ('_', align_seq[align_i+j])
-                    else:
-                        mut_key = ('_', rc(align_seq[align_i+j]))
-                    mutation_profile[mut_key] = mutation_profile.get(mut_key,0) + 1
-                align_i += nt_count
+                        # mutations
+                        if ref_seq[ref_i+j] != align_seq[align_i+j]:
+                            if aligned_read.is_reverse:
+                                mut_key = (rc(ref_seq[ref_i+j]), rc(align_seq[align_i+j]))
+                            else:
+                                mut_key = (ref_seq[ref_i+j], align_seq[align_i+j])
+                            mutation_profile[mut_key] = mutation_profile.get(mut_key,0) + 1/nh_tag
 
-            # deletion
-            elif code == 'D':
-                for j in range(nt_count):
-                    if strand == '+':
-                        mut_key = (ref_seq[ref_i+j], '_')
-                    else:
-                        mut_key = (rc(ref_seq[ref_i+j]), '_')
-                    mutation_profile[mut_key] = mutation_profile.get(mut_key,0) + 1
-                ref_i += nt_count
+                    # update indexes
+                    ref_i += nt_count
+                    align_i += nt_count
 
-            # intron
-            elif code == 'N':
-                ref_i += nt_count
+                # insertion
+                elif code == 1:
+                    # mutations
+                    for j in range(nt_count):
+                        if aligned_read.is_reverse:
+                            mut_key = ('_', rc(align_seq[align_i+j]))
+                        else:
+                            mut_key = ('_', align_seq[align_i+j])
+                        mutation_profile[mut_key] = mutation_profile.get(mut_key,0) + 1/nh_tag
+
+                    # update indexes
+                    align_i += nt_count
+
+                # deletion
+                elif code == 2:
+                    # mutations
+                    for j in range(nt_count):
+                        if aligned_read.is_reverse:
+                            mut_key = (rc(ref_seq[ref_i+j]), '_')
+                        else:
+                            mut_key = (ref_seq[ref_i+j], '_')
+
+                    # update indexes
+                    ref_i += nt_count
+
+                # intron
+                elif code == 3:
+                    # update indexes
+                    ref_i += nt_count
+
+    # clean
+    bam_in.close()
+    if options.feature_file:
+        os.close(bam_feat_fd)
+        os.remove(bam_feat_file)
 
     # print acgt content
     print 'ACGT content:'
@@ -120,16 +128,6 @@ def main():
     print 'Normalized mutations:'
     norm_mutation_profile = normalize_profile(mutation_profile, acgt_content)
     print_table(norm_mutation_profile)
-
-
-################################################################################
-# get_strand
-################################################################################
-def get_strand(flag):
-    strand = "+"
-    if flag & (0x10):	# minus strand if true.
-        strand = "-"		
-    return strand
 
 
 ################################################################################
@@ -156,23 +154,6 @@ def normalize_profile(mutation_profile, acgt_content):
             norm_mutation_profile[(nt1,nt2)] = factor*mutation_profile.get((nt1,nt2),0)
 
     return norm_mutation_profile
-
-
-################################################################################
-# parse_cigar
-#
-# Return a list of (nt count, cigar code) tuples from a cigar string.
-################################################################################
-def parse_cigar(cigar_str):
-    cigar_a = []
-    nt_count = 0
-    for i in range(len(cigar_str)):
-        if cigar_str[i].isdigit():
-            nt_count = 10*nt_count + int(cigar_str[i])
-        else:
-            cigar_a.append((nt_count,cigar_str[i]))
-            nt_count = 0
-    return cigar_a
 
 
 ################################################################################
@@ -205,5 +186,5 @@ def rc(seq):
 # __main__
 ################################################################################
 if __name__ == '__main__':
-    main()
-    #pdb.runcall(main)
+    #main()
+    pdb.runcall(main)
